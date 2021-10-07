61be8b3e4890450647d28d123c7576e07dd085fb
Johannes Polzer <johannes.polzer@outlook.at>
2021-10-07 21:25:01 +0200

    Create 404 page (#12)

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -3,6 +3,7 @@ import { initializeComponents } from "./loader";
 import router from "./router";
 
 import Test from "./components/Test";
+import NotFoundError from "./components/404";
 
 const app = document.querySelector("#app")!;
 
@@ -12,12 +13,22 @@ let components: ComponentInfo[] = [
         type: Test,
         path: "/Test.html",
     },
+    {
+        tagName: "not-found",
+        type: NotFoundError,
+        path: "/404.html",
+    },
 ];
 
 initializeComponents(components).then(main);
 
 function main() {
     router.on("/", rootPage);
+    router.on("404", notFoundPage);
+}
+function notFoundPage() {
+    const testComponent = document.createElement("not-found");
+    app.append(testComponent);
 }
 function rootPage() {
     const testComponent = document.createElement("test-component");
