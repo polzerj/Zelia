301883ec6c5c513368443aef28b15c304f4bf9c8
Johannes Polzer <johannes.polzer@outlook.at>
2021-10-16 09:21:26 +0200

    Reformat code

--- api/src/controllers/TimeTable.ts
+++ api/src/controllers/TimeTable.ts
@@ -1,18 +1,13 @@
-import { ControllerBase,Request,Response } from "../types";
-import {getTimetableByRoomNumber} from "../services/WebUntis"
+import { ControllerBase, Request, Response } from "../types";
+import { getTimetableByRoomNumber } from "../services/WebUntis";
 
-export default class Timetable extends ControllerBase
-{
-
-    constructor()
-    {
-
-        super("/room/:roomNr/timetable")
+export default class Timetable extends ControllerBase {
+    constructor() {
+        super("/room/:roomNr/timetable");
     }
-    async get(req:Request, res: Response)
-    {
-        let roomnumber = req.params.roomNr;
-        let timetable = await getTimetableByRoomNumber(roomnumber)
+    async get(req: Request, res: Response) {
+        let roomNumber = req.params.roomNr;
+        let timetable = await getTimetableByRoomNumber(roomNumber);
         res.json(timetable);
     }
 }
