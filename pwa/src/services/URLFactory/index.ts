67930e5b8441745ab7e7f0107c8423501f1e1180
Johannes Polzer <johannes.polzer@outlook.at>
2021-10-17 16:39:40 +0200

    Create get room list function

--- pwa/src/services/URLFactory/index.ts
+++ pwa/src/services/URLFactory/index.ts
@@ -4,3 +4,7 @@ const baseurl = "http://localhost:5000/api";
 export function getTimetableUrl(roomNr: string) {
     return `${baseurl}/room/${roomNr}/timetable`;
 }
+
+export function gerRoomListUrl() {
+    return `${baseurl}/room/`;
+}
