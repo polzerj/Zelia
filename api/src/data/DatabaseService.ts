41b8e20ee2ada061ef4d77490537375b41b103c1
gandalf129 <richard.panzer29@gmail.com>
2021-10-18 21:36:52 +0200

    Implement functions for the rest of the api

--- api/src/data/DatabaseService.ts
+++ api/src/data/DatabaseService.ts
@@ -1,11 +1,15 @@
 import RoomEntity from "./entities/RoomEntity";
 import RoomReportEntity from "./entities/RoomReportEntity";
+import {getRooms} from "./RoomConnection";
+import {getRoomReports} from "./RoomReportConnection";
 
 async function getRoomInfoByRoomNumber(roomNumber: string):Promise<RoomEntity[]> {
-    const data: RoomEntity[] = [{Id:1, RoomNumber:'2223', LongName: '2AHITN',HasABeamer:true, HasTeacherComputer:true, HasWater:true, IsWheelchairAccessable:true, NumberOfComputers:3, Seatplaces: 15, AdminUserId:3}]
-    return await Promise.resolve(data);
+    const data: RoomEntity[] = await getRooms(roomNumber);
+    console.log(data);
+    return data;
+};
+async function getRoomReportByRoomNumber(roomNumber: string):Promise<RoomReportEntity[]> {
+    const data: RoomReportEntity[] = await getRoomReports(roomNumber);
+    console.log(data);
+    return data;
 }
-
-//async function getRoomReportByRoomNumber(roomNumber: string):Promise<RoomReportEntity[]> {
-//    const data: RoomReportEntity[] = [{Id:1, }]
-//}
