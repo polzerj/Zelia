dcad7c36badde24f2a06cb6683cfe0fec2cbb057
Julian <julian.kusternigg@gmail.com>
2021-10-21 23:21:47 +0200

    Refactor room info component

--- pwa/src/components/RoomInfo.ts
+++ pwa/src/components/RoomInfo.ts
@@ -1,5 +1,7 @@
+import { getRoomInfoByRoomNumber } from "../services/roominfo";
 import RoomInfoModel from "../services/roominfo/RoomInfoModel";
 import Component from "../types/Component";
+import logger from "../util/logger";
 
 interface SearchElements {
     details: HTMLDetailsElement;
@@ -10,34 +12,57 @@ export default class RoomInfo extends Component<SearchElements> {
     private _roomInfo?: RoomInfoModel;
 
     constructor() {
-        super("zelia-room-info", {
-            details: "#dtlInfo",
-            divExtendable: "#divExtendable",
-        });
+        super(
+            "zelia-room-info",
+            {
+                details: "#dtlInfo",
+                divExtendable: "#divExtendable",
+            },
+            false
+        );
+
+        let attr = this.getAttribute("room-number");
+        if (attr) this.loadInfo(attr);
     }
 
-    set roomInfo(roomInfo: RoomInfoModel) {
-        this._roomInfo = roomInfo;
+    set roomNumber(roomNumber: string) {
+        this.setAttribute("room-number", roomNumber);
+
+        this.loadInfo(roomNumber);
     }
 
-    connectedCallback() {
-        if (this._roomInfo) {
-            let welcomeMsg = "Willkommen in " + this._roomInfo.roomNumber;
+    get roomNumber(): string {
+        return this.getAttribute("room-number") ?? "";
+    }
 
-            this.setState("welcomeMsg", welcomeMsg);
-            this.setState("infoSummary", this._roomInfo.description);
-
-            for (const key in this._roomInfo) {
-                let e = document.createElement("p");
-                let val = (this._roomInfo as any)[key];
-
-                e.textContent = key + " - " + val;
-
-                this.elements.divExtendable.append(e);
-            }
-        } else {
-            this.setState("welcomeMsg", "Raum nicht gefunden :(");
-            this.elements.details.classList.add("hidden");
+    private async loadInfo(roomNumber: string) {
+        try {
+            let roomInfo = await getRoomInfoByRoomNumber(roomNumber);
+            this.createElements(roomInfo);
+        } catch (e) {
+            logger.error(e);
+            this.createErrorElements();
         }
+    }
+
+    private createElements(roomInfo: RoomInfoModel) {
+        let welcomeMsg = "Willkommen in " + roomInfo.roomNumber;
+
+        this.setState("welcomeMsg", welcomeMsg);
+        this.setState("infoSummary", roomInfo.description);
+
+        for (const key in roomInfo) {
+            let e = document.createElement("p");
+            let val = (roomInfo as any)[key];
+
+            e.textContent = key + " - " + val;
+
+            this.elements.divExtendable.append(e);
+        }
+    }
+
+    private createErrorElements() {
+        this.setState("welcomeMsg", "Raum nicht gefunden :(");
+        this.elements.details.classList.add("hidden");
     }
 }
