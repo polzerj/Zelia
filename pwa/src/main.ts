ab75c1f423feab744f330056affe7dad61e7fe45
ZupaJul <44403676+juiian7@users.noreply.github.com>
2021-10-21 23:01:34 +0200

    Create room info component (#44)

--- pwa/src/main.ts
+++ pwa/src/main.ts
@@ -1,12 +1,14 @@
 import ComponentInfo from "./types/ComponentInfo";
 import { initializeComponents } from "./loader";
 import router, { PathVariables } from "./router";
+import { getRoomInfoByRoomNumber } from "./services/roominfo";
 
 import Test from "./components/Test";
 import NotFoundError from "./components/404";
 import OCR from "./components/OCR";
 import logger from "./util/logger";
 import Link from "./components/Link";
+import RoomInfo from "./components/RoomInfo";
 import RoomInput from "./components/RoomInput";
 import Timetable from "./components/Timetable";
 
@@ -32,6 +34,11 @@ let components: ComponentInfo[] = [
         tagName: "zelia-link",
         type: Link,
         path: "/Link.html",
+    },
+    {
+        tagName: "zelia-room-info",
+        type: RoomInfo,
+        path: "/RoomInfo.html",
     },
     {
         tagName: "zelia-room-input",
@@ -73,10 +80,28 @@ function ocrPage() {
     app.append(ocr);
 }
 
-function roomPage(variables?: PathVariables) {
+async function roomPage(variables?: PathVariables) {
     logger.info(variables?.roomNumber);
+  
+    const backLink = document.createElement("zelia-link") as Link;
+    backLink.textContent = "<- Back";
+    backLink.href = "/";
+    app.append(backLink);
 
     const timetable = document.createElement("zelia-timetable") as Timetable;
     if (variables?.roomNumber) timetable.roomNumber = variables?.roomNumber;
     app.append(timetable);
+  
+  const info = document.createElement("zelia-room-info") as RoomInfo;
+    
+    if (variables?.roomNumber) {
+        try {
+            info.roomInfo = await getRoomInfoByRoomNumber(
+                variables?.roomNumber
+            );
+        } catch (e) {
+            logger.error(e);
+        }
+    }
+    app.append(info);
 }
