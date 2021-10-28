860f21d519fbdad02df40a35f22b67097fc5d714
Mersed06 <44401794+Mersed06@users.noreply.github.com>
2021-10-28 16:52:02 +0200

    Implement Error Handling for the API (#71)
    Co-authored-by: mariomaroni <MarioNaunovic@users.noreply.github.com>

--- api/src/data/DatabaseService.ts
+++ api/src/data/DatabaseService.ts
@@ -10,29 +10,35 @@ import { getRoomReservations } from "./RooomReservationConnection";
 import { getLessons } from "./LessonConnection";
 import { getAdminUser } from "./AdminUserConnection";
 
-export async function getRoomInfoByRoomNumber(roomNumber: string): Promise<RoomEntity[]> {
-  const data: RoomEntity[] = await getRooms(roomNumber);
-  return data;
+export async function getRoomInfoByRoomNumber(
+    roomNumber: string
+): Promise<RoomEntity[]> {
+    const data: RoomEntity[] = await getRooms(roomNumber);
+    return data;
 }
 
-export async function getRoomReportByRoomNumber(roomNumber: string): Promise<RoomReportEntity[]> {
-  const data = await getRoomReports(roomNumber);
-  return data;
+export async function getRoomReportByRoomNumber(
+    roomNumber: string
+): Promise<RoomReportEntity[]> {
+    const data = await getRoomReports(roomNumber);
+    return data;
 }
 
 export async function getRoomReservationByRoomNumber(
-  roomNumber: string
+    roomNumber: string
 ): Promise<RoomReservationEntity[]> {
-  const data = await getRoomReservations(roomNumber);
-  return data;
+    const data = await getRoomReservations(roomNumber);
+    return data;
 }
 
-export async function getLessonByRoomNumber(roomNumber: string): Promise<LessonEntity[]> {
-  const data = await getLessons(roomNumber);
-  return data;
+export async function getLessonByRoomNumber(
+    roomNumber: string
+): Promise<LessonEntity[]> {
+    const data = await getLessons(roomNumber);
+    return data;
 }
 
 export async function getAdminUsers(): Promise<AdminUserEntity[]> {
-  const data = await getAdminUser();
-  return data;
+    const data = await getAdminUser();
+    return data;
 }
