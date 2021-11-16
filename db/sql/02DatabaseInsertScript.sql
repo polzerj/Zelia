048be4d56257953b75d35be416b0c7709f5074a5
richardp29 <44403733+richardp29@users.noreply.github.com>
2021-11-16 09:17:23 +0100

    Create Docker Support (#91)

---
+++ db/sql/02DatabaseInsertScript.sql
@@ -0,0 +1,31 @@
+USE Zelia;
+
+INSERT INTO
+    AdminUser (UserName, UserPassword)
+VALUES
+    ('Richard', 'Blabla'),
+    ('Johannes', 'Blabla');
+
+INSERT INTO 
+    Room (AdminUserId, RoomNumber, LongName, RoomDescription, RoomType, IsWheelchairAccessable, HasWater, HasTeacherComputer, Projector, ProjectorConnectors, Boards, NumberOfComputers, NumberOfSeats)
+VALUES
+    (1, 'S1308', '5AHITN', 'Klassenraum der 5A/BHITN', 'Class', true, false, true, 'normal', 'HDMI, VGA', 'normal', 1, 26),
+    (2, 'S2406', 'EDV-2406', 'EDV-Saal', 'EDV', true, true, true, 'smart', 'HDMI', 'Whiteboard', 1, 30);
+
+INSERT INTO
+    RoomReport (RoomId, AssignedAdminId, ReportDescription, Email, ReportDateTime, ReportStatus)
+VALUES
+    (1, 1, 'Left Lamp', 'richard.panzer@edu.szu.at', '2021-10-21 14:24:00', 'In progress'),
+    (2, 1, 'Right Lamp', 'johannes.polzer@edu.szu.at', '2021-10-21 15:58:00', 'Done');
+
+INSERT INTO
+    RoomReservation (RoomId, AssignedAdminId, ReservationReason, Email, StartReservation, EndReservation, ReservationStatus)
+VALUES
+    (1, 1, 'Private Lesson Math', 'richard.panzer@edu.szu.at', '2021-10-21 15:35:00', '2021-10-21 16:25:00', 'In Progress'),
+    (2, 1, 'Private Lesson German', 'johannes.polzer@edu.szu.at', '2021-10-21 16:25:00', '2021-10-21 17:15:00', 'approved');
+
+INSERT INTO
+    Lesson (RoomId, CurrentClass, CurrentTeacher, CurrentSubject, StartLesson, EndLesson)
+VALUES
+    (1, '2AHITN', 'KURZ', 'D', '2021-10-21 08:15:00', '2021-10-21 09:05:00'),
+    (2, '2BHITN', 'SCPA', 'E', '2021-10-21 08:15:00', '2021-10-21 09:05:00');
