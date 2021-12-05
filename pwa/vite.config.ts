6b60b281691bc188f6ec2cb847d7d059750a3849
Julian <julian.kusternigg@gmail.com>
2021-12-05 13:37:39 +0100

    Start implemtation

--- pwa/vite.config.ts
+++ pwa/vite.config.ts
@@ -5,9 +5,16 @@ export default defineConfig({
     publicDir: "./public",
     build: {
         outDir: "../dist",
+        rollupOptions: {
+            input: {
+                index: "./src/index.html",
+                admin: "./src/admin.html",
+            },
+        },
+        emptyOutDir: true,
     },
     server: {
         https: true,
         host: "0.0.0.0",
-    }
+    },
 });
