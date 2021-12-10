27e15fa5de5d197de21831c0766bc51796f4e415
richardp29 <44403733+richardp29@users.noreply.github.com>
2021-12-10 08:51:45 +0100

    Update database scripts (#127)

--- db/sql/02DatabaseInsertScript.sql
+++ db/sql/02DatabaseInsertScript.sql
@@ -13,16 +13,16 @@ VALUES
     (2, 'S2406', 'EDV-2406', 'EDV-Saal', 'EDV', true, true, true, 'smart', 'HDMI', 'Whiteboard', 1, 30);
 
 INSERT INTO
-    RoomReport (RoomId, AssignedAdminId, ReportDescription, Email, ReportDateTime, ReportStatus)
+    RoomReport (RoomId, AssignedAdminId, ReportDescription, Email, ReportDateTime, ReportStatus, Hash, Verified)
 VALUES
-    (1, 1, 'Left Lamp', 'richard.panzer@edu.szu.at', '2021-10-21 14:24:00', 'In progress'),
-    (2, 1, 'Right Lamp', 'johannes.polzer@edu.szu.at', '2021-10-21 15:58:00', 'Done');
+    (1, 1, 'Left Lamp', 'richard.panzer@edu.szu.at', '2021-10-21 14:24:00', 'In progress', 'asdfg', false),
+    (2, 1, 'Right Lamp', 'johannes.polzer@edu.szu.at', '2021-10-21 15:58:00', 'Done', 'lkjhg', false);
 
 INSERT INTO
-    RoomReservation (RoomId, AssignedAdminId, ReservationReason, Email, StartReservation, EndReservation, ReservationStatus)
+    RoomReservation (RoomId, AssignedAdminId, ReservationReason, Email, StartReservation, EndReservation, ReservationStatus, Hash, Verified)
 VALUES
-    (1, 1, 'Private Lesson Math', 'richard.panzer@edu.szu.at', '2021-10-21 15:35:00', '2021-10-21 16:25:00', 'In Progress'),
-    (2, 1, 'Private Lesson German', 'johannes.polzer@edu.szu.at', '2021-10-21 16:25:00', '2021-10-21 17:15:00', 'approved');
+    (1, 1, 'Private Lesson Math', 'richard.panzer@edu.szu.at', '2021-10-21 15:35:00', '2021-10-21 16:25:00', 'In Progress', 'asdfg', false),
+    (2, 1, 'Private Lesson German', 'johannes.polzer@edu.szu.at', '2021-10-21 16:25:00', '2021-10-21 17:15:00', 'approved', 'lkjhg', false);
 
 INSERT INTO
     Lesson (RoomId, CurrentClass, CurrentTeacher, CurrentSubject, StartLesson, EndLesson)
