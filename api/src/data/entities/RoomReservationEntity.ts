78f689245c22593e09264aa5292e950467580f99
ZupaJul <44403676+juiian7@users.noreply.github.com>
2021-12-10 11:12:17 +0100

    Create Room Booking Component for PWA (#128)

--- api/src/data/entities/RoomReservationEntity.ts
+++ api/src/data/entities/RoomReservationEntity.ts
@@ -1,12 +1,12 @@
 export default interface RoomReservationEntity {
-  Id?: number;
-  RoomId: number;
-  AssignedAdminId: number;
-  ReservationReason: string;
-  Email: string;
-  StartReservation: Date;
-  EndReservation: Date;
-  ReservationStatus: string;
-  Hash: string;
-  Verified: boolean;
+    Id?: number;
+    RoomId: number;
+    AssignedAdminId: number;
+    ReservationReason: string;
+    Email: string;
+    StartReservation: Date;
+    EndReservation: Date;
+    ReservationStatus: string;
+    Hash: string;
+    Verified: boolean;
 }
