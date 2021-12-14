187a2843144d140553a95d192b3be5d98cc42261
richardp29 <44403733+richardp29@users.noreply.github.com>
2021-12-14 11:10:35 +0100

    Implement an interface to return all RoomBookings (#137)

--- api/src/data/DatabaseService.ts
+++ api/src/data/DatabaseService.ts
@@ -7,7 +7,12 @@ import Booking from "../types/Booking";
 
 import { getRooms, Room } from "./RoomConnection";
 import { getRoomReports, setRoomReport, RoomReport } from "./RoomReportConnection";
-import { getRoomReservations, setRoomReservation, RoomReservation } from "./RoomReservationConnection";
+import {
+  getRoomReservation,
+  getRoomReservations,
+  setRoomReservation,
+  RoomReservation,
+} from "./RoomReservationConnection";
 import { getLessons, Lesson } from "./LessonConnection";
 import { getAdminUser, AdminUser } from "./AdminUserConnection";
 import { RoomNotFoundException } from "./Exceptions/RoomNotFoundException";
@@ -15,86 +20,105 @@ import { DatabaseNotAvailableException } from "./Exceptions/DatabaseNotAvailable
 import { NoAdminUsersFoundException } from "./Exceptions/NoAdminUsersFoundException";
 import { CouldNotInsertDataException } from "./Exceptions/CouldNotInsertDataException";
 import Report from "types/Report";
+import { NoRoomReservationsFoundException } from "./Exceptions/NoRoomReservationsFoundException";
 
 export async function getRoomInfoByRoomNumber(roomNumber: string): Promise<RoomEntity[]> {
-    let data: Room[];
-    try {
-        data = await getRooms(roomNumber);
-    } catch (e) {
-        throw new DatabaseNotAvailableException();
-    }
-    if (data.length === 0) {
-        throw new RoomNotFoundException();
-    }
-    return data;
+  let data: Room[];
+  try {
+    data = await getRooms(roomNumber);
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  if (data.length === 0) {
+    throw new RoomNotFoundException();
+  }
+  return data;
 }
 
 export async function getRoomReportByRoomNumber(roomNumber: string): Promise<RoomReportEntity[]> {
-    let data: RoomReport[];
-    try {
-        data = await getRoomReports(roomNumber);
-    } catch (e) {
-        throw new DatabaseNotAvailableException();
-    }
-    if (data.length === 0) {
-        throw new RoomNotFoundException();
-    }
-    return data;
+  let data: RoomReport[];
+  try {
+    data = await getRoomReports(roomNumber);
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  if (data.length === 0) {
+    throw new RoomNotFoundException();
+  }
+  return data;
 }
 
-export async function getRoomReservationByRoomNumber(roomNumber: string): Promise<RoomReservationEntity[]> {
-    let data: RoomReservation[];
-    try {
-        data = await getRoomReservations(roomNumber);
-    } catch (e) {
-        throw new DatabaseNotAvailableException();
-    }
-    if (data.length === 0) {
-        throw new RoomNotFoundException();
-    }
-    return data;
+export async function getRoomReservationByRoomNumber(
+  roomNumber: string
+): Promise<RoomReservationEntity[]> {
+  let data: RoomReservation[];
+  try {
+    data = await getRoomReservation(roomNumber);
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  if (data.length === 0) {
+    throw new RoomNotFoundException();
+  }
+  return data;
+}
+
+export async function getAllRoomReservations(): Promise<RoomReservationEntity[]> {
+  let data: RoomReservation[];
+  try {
+    data = await getRoomReservations();
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  if (data.length === 0) {
+    throw new NoRoomReservationsFoundException();
+  }
+  return data;
 }
 
 export async function getLessonByRoomNumber(roomNumber: string): Promise<LessonEntity[]> {
-    let data: Lesson[];
-    try {
-        data = await getLessons(roomNumber);
-    } catch (e) {
-        throw new DatabaseNotAvailableException();
-    }
-    if (data.length === 0) {
-        throw new RoomNotFoundException();
-    }
-    return data;
+  let data: Lesson[];
+  try {
+    data = await getLessons(roomNumber);
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  if (data.length === 0) {
+    throw new RoomNotFoundException();
+  }
+  return data;
 }
 
-export async function getAdminUserByNameAndPw(userName: string, hash: string): Promise<AdminUserEntity> {
-    let data: AdminUser;
-    try {
-        data = await getAdminUser(userName, hash);
-    } catch (e) {
-        throw new DatabaseNotAvailableException();
-    }
-    console.log(data);
+export async function getAdminUserByNameAndPw(
+  userName: string,
+  hash: string
+): Promise<AdminUserEntity> {
+  let data: AdminUser;
+  try {
+    data = await getAdminUser(userName, hash);
+  } catch (e) {
+    throw new DatabaseNotAvailableException();
+  }
+  console.log(data);
 
-    if (data === null) {
-        throw new NoAdminUsersFoundException();
-    }
-    return data;
+  if (data === null) {
+    throw new NoAdminUsersFoundException();
+  }
+  return data;
 }
 
 export async function setRoomReservationByDate(booking: Booking) {
-    try {
-        setRoomReservation(booking);
-    } catch (e) {
-        throw new CouldNotInsertDataException();
-    }
+  try {
+    setRoomReservation(booking);
+  } catch (e) {
+    throw new CouldNotInsertDataException();
+  }
 }
 
 export async function setRoomReportDbService(roomReport: Report) {
-    try {
-        setRoomReport(roomReport);
-    } catch (e) {
-        throw new CouldNotInsertDataException();
-    }
+  try {
+    setRoomReport(roomReport);
+  } catch (e) {
+    throw new CouldNotInsertDataException();
+  }
 }
--- api/src/data/RoomReservationConnection.ts
+++ api/src/data/RoomReservationConnection.ts
@@ -1,15 +1,15 @@
 import {
-    Sequelize,
-    Model,
-    ModelDefined,
-    DataTypes,
-    HasManyGetAssociationsMixin,
-    HasManyAddAssociationMixin,
-    HasManyHasAssociationMixin,
-    Association,
-    HasManyCountAssociationsMixin,
-    HasManyCreateAssociationMixin,
-    Optional,
+  Sequelize,
+  Model,
+  ModelDefined,
+  DataTypes,
+  HasManyGetAssociationsMixin,
+  HasManyAddAssociationMixin,
+  HasManyHasAssociationMixin,
+  Association,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  Optional,
 } from "sequelize";
 
 import RoomReservationEntity from "./entities/RoomReservationEntity";
@@ -21,98 +21,110 @@ import { getRoomInfoByRoomNumber } from "./DatabaseService";
 import RoomBooking from "controllers/RoomBooking";
 
 export class RoomReservation extends Model<RoomReservationEntity> implements RoomReservationEntity {
-    public Id?: number;
-    public RoomId!: number;
-    public AssignedAdminId!: number;
-    public ReservationReason!: string;
-    public Email!: string;
-    public StartReservation!: Date;
-    public EndReservation!: Date;
-    public ReservationStatus!: string;
-    public Hash!: string;
-    public Verified!: boolean;
+  public Id?: number;
+  public RoomId!: number;
+  public AssignedAdminId!: number;
+  public ReservationReason!: string;
+  public Email!: string;
+  public StartReservation!: Date;
+  public EndReservation!: Date;
+  public ReservationStatus!: string;
+  public Hash!: string;
+  public Verified!: boolean;
 }
 
 RoomReservation.init(
-    {
-        Id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-        },
-        RoomId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: Room,
-                key: "RoomId",
-            },
-        },
-        AssignedAdminId: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        ReservationReason: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        Email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        StartReservation: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        EndReservation: {
-            type: DataTypes.DATE,
-            allowNull: false,
-        },
-        ReservationStatus: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        Hash: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        Verified: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false,
-        },
+  {
+    Id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
-    {
-        tableName: "RoomReservation",
-        sequelize,
-    }
+    RoomId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Room,
+        key: "RoomId",
+      },
+    },
+    AssignedAdminId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    ReservationReason: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    StartReservation: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    EndReservation: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    ReservationStatus: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Hash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Verified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "RoomReservation",
+    sequelize,
+  }
 );
 
 Room.hasMany(RoomReservation);
 RoomReservation.belongsTo(Room, { foreignKey: "RoomId" });
 
-export async function getRoomReservations(roomNumber: string): Promise<RoomReservation[]> {
-    const roomReservation = await RoomReservation.findAll({
-        include: [
-            {
-                model: Room,
-                required: true,
-                where: { RoomNumber: roomNumber },
-            },
-        ],
-    });
-    return roomReservation;
+export async function getRoomReservation(roomNumber: string): Promise<RoomReservation[]> {
+  const roomReservation = await RoomReservation.findAll({
+    include: [
+      {
+        model: Room,
+        required: true,
+        where: { RoomNumber: roomNumber },
+      },
+    ],
+  });
+  return roomReservation;
 }
 
 export async function setRoomReservation(roomBooking: Booking) {
-    let roomId = (await getRoomInfoByRoomNumber(roomBooking.roomNumber))[0].Id;
-    RoomReservation.create({
-        RoomId: roomId,
-        AssignedAdminId: 1,
-        ReservationReason: roomBooking.purpose,
-        Email: roomBooking.user,
-        // TODO: Not going to work :)
-        StartReservation: new Date(roomBooking.from),
-        EndReservation: new Date(roomBooking.until),
-        ReservationStatus: "open",
-        Hash: roomBooking.hash,
-        Verified: false,
-    });
+  let roomId = (await getRoomInfoByRoomNumber(roomBooking.roomNumber))[0].Id;
+  RoomReservation.create({
+    RoomId: roomId,
+    AssignedAdminId: 1,
+    ReservationReason: roomBooking.purpose,
+    Email: roomBooking.user,
+    // TODO: Not going to work :)
+    StartReservation: new Date(roomBooking.from),
+    EndReservation: new Date(roomBooking.until),
+    ReservationStatus: "open",
+    Hash: roomBooking.hash,
+    Verified: false,
+  });
+}
+
+export async function getRoomReservations(): Promise<RoomReservation[]> {
+  const roomReservations = await RoomReservation.findAll({
+    include: [
+      {
+        model: Room,
+        required: true,
+      },
+    ],
+  });
+  return roomReservations;
 }
