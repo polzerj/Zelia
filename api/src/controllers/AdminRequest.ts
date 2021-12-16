0ca54d6222ad676cf8c35948fa46e6476ee22d2c
ZupaJul <44403676+juiian7@users.noreply.github.com>
2021-12-16 09:35:32 +0100

    Add error handling

--- api/src/controllers/AdminRequest.ts
+++ api/src/controllers/AdminRequest.ts
@@ -2,12 +2,11 @@ import Booking from "../types/Booking";
 import Report from "../types/Report";
 import { Request, Response, ControllerBase } from "../types";
 import AuthenticationMiddleware from "../middleware/AutenticationMiddleware";
-import {
-    getAllRoomReports,
-    getAllRoomReservations,
-} from "../data/DatabaseService";
+import { getAllRoomReports, getAllRoomReservations } from "../data/DatabaseService";
 import { RoomReport } from "../data/RoomReportConnection";
 import { RoomReservation } from "../data/RoomReservationConnection";
+import { DatabaseNotAvailableException } from "../data/Exceptions/DatabaseNotAvailableException";
+import { NoAdminUsersFoundException } from "../data/Exceptions/NoAdminUsersFoundException";
 
 export default class AdminRequest extends ControllerBase {
     constructor() {
@@ -15,18 +14,30 @@ export default class AdminRequest extends ControllerBase {
     }
 
     async get(req: Request, res: Response) {
-        let reqs = [
-            ...(await getAllRoomReports()),
-            ...(await getAllRoomReservations()),
-        ];
-        let toSend = [];
-        for (const req of reqs) {
-            if (req instanceof RoomReport) {
-                toSend.push(Report.fromDB(req));
-            } else if (req instanceof RoomReservation) {
-                toSend.push(Booking.fromDB(req));
+        try {
+            let reqs = [...(await getAllRoomReports()), ...(await getAllRoomReservations())];
+
+            let toSend = [];
+            for (const req of reqs) {
+                if (req instanceof RoomReport) {
+                    toSend.push(Report.fromDB(req));
+                } else if (req instanceof RoomReservation) {
+                    toSend.push(Booking.fromDB(req));
+                }
             }
+            res.json(toSend);
+        } catch (e) {
+            if (e instanceof NoAdminUsersFoundException) {
+                res.status(401).send("Invalid Login Name|Password");
+                return;
+            }
+
+            if (e instanceof DatabaseNotAvailableException) {
+                res.status(500).send("Database not available");
+                return;
+            }
+
+            res.status(501).send("Other Server error");
         }
-        res.json(toSend);
     }
 }
