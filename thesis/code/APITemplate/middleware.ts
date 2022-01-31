c6b47aec8dba147a50625ace2ba19171b01b9387
Johannes Polzer <johannes.polzer@outlook.at>
2022-01-31 20:55:41 +0100

    Add code snippets and describe the controllerbase

---
+++ thesis/code/APITemplate/middleware.ts
@@ -0,0 +1,1 @@
+function middleware(req: Request, res: Response, next: () => void) => void {}
---
+++ thesis/code/Authentication/LoginController.ts
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export default class Login extends ControllerBase {
+  constructor() {
+    super("/admin/login");
+  }
+
+  async post(req: Request, res: Response) {
+    let user = req.body.username;
+    let pwd = req.body.password;
+    //let hash = crypto.createHash("md5").update(pwd).digest("hex");
+    try {
+      let data = await getAdminUserByNameAndPw(user, pwd);
+      let token = jwt.sign({ user }, JWT_SECRET, {
+        algorithm: "HS256",
+      });
+      res.status(200).json({ token });
+    } catch (e) {
+      if (e instanceof DatabaseNotAvailableException) {
+        res.status(500).send("Database not available");
+        return;
+      }
+      if (e instanceof NoAdminUsersFoundException) {
+        res.status(401).send("Invalid Login Name|Password");
+        return;
+      }
+    }
+  }
+}
