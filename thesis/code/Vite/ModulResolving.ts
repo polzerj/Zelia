e06d2b80c1337825e03059c0e2ac1b18872cde83
Johannes Polzer <johannes.polzer@outlook.at>
2022-02-09 17:38:42 +0100

    Implementation of vite in thesis

---
+++ thesis/code/Vite/ModulResolving.ts
@@ -0,0 +1,5 @@
+// Import of a node Module with ES2015 import syntax:
+import { createWorker, createScheduler, Scheduler } from "tesseract.js";
+
+// Resolved Modul
+import __vite__cjsImport0_tesseract_js from "/@fs/Users/polzerj/Documents/dev/Diplomarbeit/Zelia/pwa/node_modules/.vite/tesseract_js.js?v=13fa957f";
