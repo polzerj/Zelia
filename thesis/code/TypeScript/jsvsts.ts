7d470b41e1e377951465b32c93ac8a2a5eee77a7
Mersed06 <mersed415@gmail.com>
2022-03-10 09:18:38 +0100

    Fixing conversation on TypeScript

--- thesis/code/TypeScript/jsvsts.ts
+++ thesis/code/TypeScript/jsvsts.ts
@@ -1,11 +1,11 @@
 // What differs typescript from javascript is the strict usage of types.
-// Javascript is a loosely typed language, 
+// Javascript is a loosely typed language,
 // while Typescript is strictly typed.
 
 // Javascript
-let car = 'BMW';
+let car = "SZU";
 let age = 15;
 
 // Typescript
-let car: string = 'BMW';
+let car: string = "SZU";
 let age: number = 15;
