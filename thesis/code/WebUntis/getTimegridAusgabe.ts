d88d6ffc63b3104c8f7acd4a305e8e95b0ffd984
Mersed06 <mersed415@gmail.com>
2022-03-24 11:54:32 +0100

    Gemeinsames Feedback für Kapitel

--- thesis/code/WebUntis/getTimegridAusgabe.ts
+++ thesis/code/WebUntis/getTimegridAusgabe.ts
@@ -1,13 +1,19 @@
-[{"day":2,
-"timeUnits":[{"name":"0","startTime":725,"endTime":815},
-{"name":"1","startTime":815,"endTime":905},
-{"name":"2","startTime":905,"endTime":955},
-{"name":"3","startTime":1015,"endTime":1105},
-{"name":"4","startTime":1105,"endTime":1155},
-{"name":"5","startTime":1205,"endTime":1255},
-{"name":"6","startTime":1255,"endTime":1345},
-{"name":"7","startTime":1345,"endTime":1435},
-{"name":"8","startTime":1435,"endTime":1525},
-{"name":"9","startTime":1535,"endTime":1625},
-{"name":"10","startTime":1625,"endTime":1715},
-{"name":"11","startTime":1715,"endTime":1805}]}]
+[
+  {
+    day: 2,
+    timeUnits: [
+      { name: "0", startTime: 725, endTime: 815 },
+      { name: "1", startTime: 815, endTime: 905 },
+      { name: "2", startTime: 905, endTime: 955 },
+      { name: "3", startTime: 1015, endTime: 1105 },
+      { name: "4", startTime: 1105, endTime: 1155 },
+      { name: "5", startTime: 1205, endTime: 1255 },
+      { name: "6", startTime: 1255, endTime: 1345 },
+      { name: "7", startTime: 1345, endTime: 1435 },
+      { name: "8", startTime: 1435, endTime: 1525 },
+      { name: "9", startTime: 1535, endTime: 1625 },
+      { name: "10", startTime: 1625, endTime: 1715 },
+      { name: "11", startTime: 1715, endTime: 1805 },
+    ],
+  },
+];
