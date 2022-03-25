98bd32f9287ddf00a1c44cde3ae996f8465855b4
Johannes Polzer <johannes.polzer@outlook.at>
2022-03-25 14:48:10 +0100

    Final Commit!!!

--- thesis/code/WebUntis/getTimetableForAusgabe.ts
+++ thesis/code/WebUntis/getTimetableForAusgabe.ts
@@ -1,2 +1,38 @@
-[{"id":1127532,"date":20220124,"startTime":1015,"endTime":1105,"kl":[{"id":993,"name":"5BHITN","longname":"HTL IT-NWTK"}],"te":[{"id":173,"name":"THOM","longname":"THOMASO"}],"su":
-[{"id":381,"name":"AM","longname":"ANGEWANDTE.MATH."}],"ro":[{"id":283,"name":"1308","longname":"5AHITN / 5BHITN"}],"lsnumber":78000,"activityType":"Unterricht"},{"id":1131147,"date":20220124,"startTime":1205,"endTime":1255,"kl":[{"id":988,"name":"5AHITN","longname":"HTL IT-NWTK"}],"te":[{"id":60,"name":"KURZ","longname":"KURZ-HOCHWARTER"}]
+[
+  {
+    id: 1127532,
+    date: 20220124,
+    startTime: 1015,
+    endTime: 1105,
+    kl: [
+      {
+        id: 993,
+        name: "5AHITN",
+        longname: "HTL IT-NWTK"
+      }
+    ],
+    te: [
+      {
+        id: 173,
+        name: "KECO",
+        longname: "KECO"
+      }
+    ]
+    su: [
+      {
+        id: 381,
+        name: "AM",
+        longname: "ANGEWANDTE.MATH."
+      }
+    ]
+    ro: [
+      {
+        id: 283,
+        name: "1308",
+        longname: "5AHITN / 5BHITN"
+      }
+    ],
+    lsnumber: 78000,
+    activityType: "Unterricht"
+  }
+]
